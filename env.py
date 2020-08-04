cadc6496e91f24228cd395667c693a56f8035414
a-telier <Alexandra.rabi@outlook.com>
2020-08-04 19:07:13 +0000

    test deployment to heroku

--- env.py
+++ env.py
@@ -3,3 +3,6 @@ import os
 os.environ['MONGO_URI'] = 'mongodb+srv://alex-rabi:Domenica16@blog.wvugx.mongodb.net/blog?retryWrites=true&w=majority'
 os.environ['SECRET_KEY'] = 'Domenica16'
 os.environ['MONGODB_NAME'] = 'blog'
+
+os.environ['IP'] = '0.0.0.0'
+os.environ['PORT'] = '8080'

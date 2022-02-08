b652ede9559b649272e851fad207e1f2dc4a0b37
a-telier <Alexandra.rabi@outlook.com>
2022-02-08 11:14:30 +0000

    debugging config file

--- env.py
+++ env.py
@@ -1,7 +1,3 @@
 import os
 
-os.environ['MONGO_URI'] = 'mongodb+srv://alex-rabi:Domenica16@blog.wvugx.mongodb.net/blog?retryWrites=true&w=majority'
-os.environ['SECRET_KEY'] = 'Pythonista3'
-os.environ['MONGODB_NAME'] = 'blog'
-
 os.environ['IP'] = '0.0.0.0'
